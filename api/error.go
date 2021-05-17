963b8dae2d0fe8cf8b50588157005a27f58e75f7
David Lilue <2525462+dvdalilue@users.noreply.github.com>
2021-05-17 23:19:52 +0000

    Error types definition

---
+++ api/error.go
@@ -0,0 +1,48 @@
+package api
+
+import (
+	"net/http"
+)
+
+type APIError struct {
+	Message string `json:"message"`
+}
+
+type ClientError struct {
+	Status  int    `json:"status"`
+	Message string `json:"message"`
+}
+
+func NewError(status int, msg string) *ClientError {
+	return &ClientError{Status: status, Message: msg}
+}
+
+func NewInternalError(msg string) *ClientError {
+	return &ClientError{Status: http.StatusInternalServerError, Message: msg}
+}
+
+func (e *ClientError) Error() string {
+	return e.Message
+}
+
+func (e *ClientError) GetStatus() int {
+	return e.Status
+}
+
+func FromError(err error) (s *ClientError, ok bool) {
+	if err == nil {
+		return nil, true
+	}
+
+	if se, ok := err.(interface {
+		Error() string
+		GetStatus() int
+	}); ok {
+		return &ClientError{Status: se.GetStatus(), Message: se.Error()}, true
+	}
+
+	return &ClientError{
+		Status:  http.StatusInternalServerError,
+		Message: err.Error(),
+	}, false
+}
