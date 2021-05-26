14629737a042dcc07bbe61055e9f3c94c95076ec
David Lilue <2525462+dvdalilue@users.noreply.github.com>
2021-05-26 15:51:03 +0000

    Documentation of API and client error types

--- api/error.go
+++ api/error.go
@@ -1,18 +1,22 @@
 package api
 
+// APIError defines the common API response for errors.
 type APIError struct {
 	Message string `json:"message"`
 }
 
+// Error defines a general error structure.
 type Error struct {
 	Code    int    `json:"code"`
 	Message string `json:"message"`
 }
 
+// NewError create a new instance of Error from a given `code` and `msg`.
 func NewError(code int, msg string) *Error {
 	return &Error{Code: code, Message: msg}
 }
 
+// Error returns a string message of the error.
 func (e *Error) Error() string {
 	return e.Message
 }
