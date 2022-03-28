8286851b4c0e28ac5db22b38d083016bc3a31a81
Sam Lown <sam.lown@invopop.com>
2022-03-28 21:40:18 +0000

    Fix connector URLs

--- invopop/transform_connectors.go
+++ invopop/transform_connectors.go
@@ -7,11 +7,11 @@ import (
 
 // CreateConnector sends a request to the API to create a new connector.
 func (svc *TransformService) CreateConnector(ctx context.Context, m *Connector) error {
-	path := fmt.Sprintf("%s/tasks/%s", transformBasePath, m.ID)
+	path := fmt.Sprintf("%s/connectors/%s", transformBasePath, m.ID)
 	return svc.client.put(ctx, path, m)
 }
 
 // ListConnectors prepares a pageable list of connectors that belong to the requester.
 func (svc *TransformService) ListConnectors(ctx context.Context, col *ConnectorCollection) error {
-	return svc.client.get(ctx, transformBasePath+"/tasks", col)
+	return svc.client.get(ctx, transformBasePath+"/connectors", col)
 }
